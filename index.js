0cbee21ffb4e19b3f195c87d9ebaf8a2888c6b4e
mahmud014 <anik955720@gmail.com>
2025-11-12 23:36:01 +0600

    add offer

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ async function run() {
     const sliderCollection = database.collection("slider_db");
     const menuCollection = database.collection("menu");
     const jobCollection = database.collection("jobs");
+    const offerCollection = database.collection("offer");
 
     // get
     app.get("/reviews", async (req, res) => {
@@ -171,6 +172,13 @@ async function run() {
     // jobCollection
     app.get("/jobs", async (req, res) => {
       const cursor = jobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // offerCollection
+
+    app.get("/offers", async (req, res) => {
+      const cursor = offerCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
